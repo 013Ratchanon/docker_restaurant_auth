81cfb23e55df4e70031ab6d1a50971227fad099d
013Ratchanon <dewtete@gmail.com>
2025-08-25 17:15:54 +0700

    middleware

--- server/routers/restaurant.router.js
+++ server/routers/restaurant.router.js
@@ -1,16 +1,32 @@
 import restaurantController from "../controllers/restaurant.controller.js";
+import authMiddleware from "../middleware/authJwt.js";
 import express from "express";
 
 const router = express.Router();
 //POST http://localhost:5000/api/v1/restaurant
-router.post("/", restaurantController.create);
+router.post(
+  "/",
+  authMiddleware.verifyToken,
+  authMiddleware.isModOrAdmin,
+  restaurantController.create
+);
 //GET http://localhost:5000/api/v1/restaurant
-router.get("/", restaurantController.getAll);
+router.get("/", authMiddleware.verifyToken, restaurantController.getAll);
 //GET http://localhost:5000/api/v1/restaurant/:id
-router.get("/:id", restaurantController.getById);
+router.get("/:id", authMiddleware.verifyToken, restaurantController.getById);
 //PUT http://localhost:5000/api/v1/restaurant/:id
-router.put("/:id", restaurantController.update);
+router.put(
+  "/:id",
+  authMiddleware.verifyToken,
+  authMiddleware.isModOrAdmin,
+  restaurantController.update
+);
 //DELETE http://localhost:5000/api/v1/restaurant/:id
-router.delete("/:id", restaurantController.delete);
+router.delete(
+  "/:id",
+  authMiddleware.verifyToken,
+  authMiddleware.isAdmin,
+  restaurantController.delete
+);
 
 export default router;
