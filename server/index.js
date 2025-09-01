90835ac5c70af37a764f16706d6676a469d771d8
013Ratchanon <dewtete@gmail.com>
2025-09-01 16:01:01 +0700

    index

--- server/index.js
+++ server/index.js
@@ -27,10 +27,10 @@ const innitRole = () => {
   role.create({ id: 2, name: "moderator" });
   role.create({ id: 3, name: "admin" });
 };
-db.sequelize.sync({ force: true }).then(() => {
-  innitRole();
-  console.log("Drop and Sync");
-});
+// db.sequelize.sync({ force: true }).then(() => {
+//   innitRole();
+//   console.log("Drop and Sync");
+// });
 
 app.get("/", (req, res) => {
   res.send("Restaurant Restful API ");
